efb88079da714816161e9700c0de813d7b87deaa
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-20 13:58:53 +0530

    created SwitchExamples  and added hands-on

---
+++ src/SwitchExamples.java
@@ -0,0 +1,108 @@
+public class SwitchExamples {
+    public static void main(String[] args) {
+        //Example 1
+        int month=7;
+        String monthString="";
+
+        switch(month){
+            //case statements within the switch block
+            case 1: monthString="1 - January";
+                break;
+            case 2: monthString="2 - February";
+                break;
+            case 3: monthString="3 - March";
+                break;
+            case 4: monthString="4 - April";
+                break;
+            case 5: monthString="5 - May";
+                break;
+            case 6: monthString="6 - June";
+                break;
+            case 7: monthString="7 - July";
+                break;
+            case 8: monthString="8 - August";
+                break;
+            case 9: monthString="9 - September";
+                break;
+            case 10: monthString="10 - October";
+                break;
+            case 11: monthString="11 - November";
+                break;
+            case 12: monthString="12 - December";
+                break;
+            default:System.out.println("Invalid Month!");
+        }
+        System.out.println(monthString);
+
+        //Example 2
+
+        String levelString="Expert";
+        int level=0;
+        //Using String in Switch expression
+        switch(levelString){
+            //Using String Literal in Switch case
+            case "Beginner": level=1;
+                break;
+            case "Intermediate": level=2;
+                break;
+            case "Expert": level=3;
+                break;
+            default: level=0;
+                break;
+        }
+        System.out.println("Your Level is: "+level);
+
+        // Example 3 #Nested Switch
+
+        char branch = 'C';
+        int collegeYear = 4;
+        switch( collegeYear )
+        {
+            case 1:
+                System.out.println("English, Maths, Science");
+                break;
+            case 2:
+                switch( branch )
+                {
+                    case 'C':
+                        System.out.println("Operating System, Java, Data Structure");
+                        break;
+                    case 'E':
+                        System.out.println("Micro processors, Logic switching theory");
+                        break;
+                    case 'M':
+                        System.out.println("Drawing, Manufacturing Machines");
+                        break;
+                }
+                break;
+            case 3:
+                switch( branch )
+                {
+                    case 'C':
+                        System.out.println("Computer Organization, MultiMedia");
+                        break;
+                    case 'E':
+                        System.out.println("Fundamentals of Logic Design, Microelectronics");
+                        break;
+                    case 'M':
+                        System.out.println("Internal Combustion Engines, Mechanical Vibration");
+                        break;
+                }
+                break;
+            case 4:
+                switch( branch )
+                {
+                    case 'C':
+                        System.out.println("Data Communication and Networks, MultiMedia");
+                        break;
+                    case 'E':
+                        System.out.println("Embedded System, Image Processing");
+                        break;
+                    case 'M':
+                        System.out.println("Production Technology, Thermal Engineering");
+                        break;
+                }
+                break;
+        }
+    }
+}
