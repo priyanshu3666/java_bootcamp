4474f43842105fa7965663f9b3eaa9d0b710cb4c
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-20 11:47:14 +0530

    created DataTypeAndVariable file

--- src/DataTypeAndVariable.java
+++ src/DataTypeAndVariable.java
@@ -1,5 +1,16 @@
+import java.security.cert.TrustAnchor;
+
 public class DataTypeAndVariable {
     public static void main(String[] args) {
-        
+        byte b =10;
+        short s =20;
+        int i = 30;
+        long long_var = 40;
+        float f =50f;
+        char c= 'A';
+        double d= 20d;
+        boolean bln = true;
+        String s1 = "Hello";
+         System.out.println(b+" "+s+" "+i+" "+long_var+" "+f+" "+c+" "+d+" "+ bln +" "+s1);
     }
 }
