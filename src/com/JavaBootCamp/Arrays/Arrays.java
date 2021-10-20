557a40f1f240a59240c609705a1b7c79ce391f88
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-20 17:38:31 +0530

    replace simple for loop wth enhanced for loop

--- src/com/JavaBootCamp/Arrays/Arrays.java
+++ src/com/JavaBootCamp/Arrays/Arrays.java
@@ -1,7 +1,7 @@
 package com.JavaBootCamp.Arrays;
 
 public class Arrays {
-    static void min(int arr[]){
+    static void min(int[] arr){
         int min=arr[0];
         for(int i=1;i<arr.length;i++)
             if(min>arr[i])
@@ -11,18 +11,16 @@ public class Arrays {
     }
 
     public static void main(String[] args) {
-        int arr[]=new int[5];
+        int[] arr =new int[5];
         arr[0]=10;
         arr[1]=20;
         arr[2]=70;
         arr[3]=40;
         arr[4]=50;
-        //traversing array
-        for(int index=0;index<arr.length;index++)//length is the property of array
-            System.out.println(arr[index]);
+        for (int i : arr) System.out.println(i);
 
         // passing array to method
-        int arr1[]={33,3,4,5};//declaring and initializing an array
+        int[] arr1 ={33,3,4,5};//declaring and initializing an array
         min(arr1);//passing array to method
     }
 }
