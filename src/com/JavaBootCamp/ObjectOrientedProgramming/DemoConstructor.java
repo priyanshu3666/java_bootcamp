7ece2540a6567f7f3762b99b26a8b5743329686e
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-21 16:54:53 +0530

    DemoConstructor created and implemented

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/DemoConstructor.java
@@ -0,0 +1,14 @@
+package com.JavaBootCamp.ObjectOrientedProgramming;
+
+public class DemoConstructor {
+    DemoConstructor() {
+        System.out.println("Bike is created");
+    }
+
+    //main method
+    public static void main(String[] args) {
+//calling a default constructor
+        new DemoConstructor();
+    }
+}
+
