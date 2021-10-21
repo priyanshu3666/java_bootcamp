7b3f0d1d8a86b023b0b58d9964bc1a7c40365982
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-21 13:39:59 +0530

    Created MyStringFormatter and add some hands-on

---
+++ src/com/JavaBootCamp/MyString/MyStringFormatter.java
@@ -0,0 +1,26 @@
+package com.JavaBootCamp.MyString;
+
+public class MyStringFormatter {
+    public static void main(String args[]){
+        String name="sonoo";
+        String sf1=String.format("name is %s",name);
+        String sf2=String.format("value is %f",32.33434);
+        String sf3=String.format("value is %32.12f",32.33434);//returns 12 char fractional part filling with 0
+        String sf4 = String.format("%x", 101);
+        String str1 = String.format("%d", 101);
+        String str2 = String.format("|%10d|", 101);  // Specifying length of integer
+        String str3 = String.format("|%-10d|", 101); // Left-justifying within the specified width
+        String str4 = String.format("|% d|", 101);
+        String str5 = String.format("|%010d|", 101);
+
+        System.out.println(sf1);
+        System.out.println(sf2);
+        System.out.println(sf3);
+        System.out.println(sf4);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
+    }
+}
