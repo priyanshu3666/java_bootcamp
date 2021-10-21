0e47d0ed86e6824b963bc612f76b9e463283114c
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-21 16:55:29 +0530

    ParametrizedConstuctor created and implemented hands-on

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/ParametrizedConstuctor.java
@@ -0,0 +1,31 @@
+package com.JavaBootCamp.ObjectOrientedProgramming;
+
+public class ParametrizedConstuctor {
+    public static void main(String[] args) {
+        //creating objects and passing values
+        Student1 s1 = new Student1(111, "Karan");
+        Student1 s2 = new Student1(222, "Aryan");
+        //calling method to display the values of object
+        s1.display();
+        s2.display();
+    }
+
+    static class  Student1 {
+        int id;
+        String name;
+
+        //creating a parameterized constructor
+        Student1(int i, String n) {
+            id = i;
+            name = n;
+        }
+
+        //method to display the values
+        void display() {
+            System.out.println(id + " " + name);
+        }
+
+
+    }
+
+}
