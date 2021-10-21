c4a873436b0440ec4820b38c0116905918d9b170
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-21 17:22:53 +0530

    refactoring of code

--- src/com/JavaBootCamp/ObjectOrientedProgramming/ConstructorOverloading.java
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/ConstructorOverloading.java
@@ -2,28 +2,34 @@ package com.JavaBootCamp.ObjectOrientedProgramming;
 
 public class ConstructorOverloading {
 
-    public static void main(String[] args){
-        Student s1 = new Student(111,"Karan");
-        Student s2 = new Student(222,"Aryan",25);
+    public static void main(String[] args) {
+        Student s1 = new Student(111, "Karan");
+        Student s2 = new Student(222, "Aryan", 25);
         s1.display();
         s2.display();
     }
+
     static class Student {
         int id;
         String name;
         int age;
+
         //creating two arg constructor
-        Student(int i, String n){
+        Student(int i, String n) {
             id = i;
             name = n;
         }
+
         //creating three arg constructor
-        Student(int i, String n, int a){
+        Student(int i, String n, int a) {
             id = i;
             name = n;
-            age=a;
+            age = a;
         }
-        void display(){System.out.println(id+" "+name+" "+age);}
+
+        void display() {
+            System.out.println(id + " " + name + " " + age);
+        }
 
     }
 }
