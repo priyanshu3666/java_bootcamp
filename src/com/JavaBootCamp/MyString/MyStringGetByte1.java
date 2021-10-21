676a53f11e6980aa8d210132109044c9a1c1e407
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-21 14:27:54 +0530

    Created MyStringGetByte1 and add some hands-on over UTF Charset supporting methods

---
+++ src/com/JavaBootCamp/MyString/MyStringGetByte1.java
@@ -0,0 +1,61 @@
+package com.JavaBootCamp.MyString;
+import java.io.*;
+
+public class MyStringGetByte1
+{
+
+    public static void main(String args[])
+    {
+        String str = "Welcome to Iedaas.";
+        System.out.println("The input String is : ");
+        System.out.println(str + "\n");
+        try
+        {
+            //16 - bit UCS Transformation format
+            byte[] byteArr = str.getBytes("UTF-16");
+            System.out.println("After converted into UTF-16 the String is : ");
+
+            for (int j = 0; j < byteArr.length; j++)
+            {
+                System.out.print(byteArr[j]);
+            }
+
+            System.out.println("\n");
+
+            // Big Endian byte order, 16 - bit UCS Transformation format
+            byte[] byteArr1 = str.getBytes("UTF-16BE");
+            System.out.println("After converted into UTF-16BE the String is : ");
+
+            for (int j = 0; j < byteArr1.length; j++)
+            {
+                System.out.print(byteArr1[j]);
+            }
+
+            System.out.println("\n");
+
+            // ISO Latin Alphabet
+            byte[] byteArr2 = str.getBytes("ISO-8859-1");
+            System.out.println("After converted into ISO-8859-1 the String is : ");
+
+            for (int j = 0; j < byteArr2.length; j++)
+            {
+                System.out.print(byteArr2[j]);
+            }
+
+            System.out.println("\n");
+
+            // Little Endian byte order, 16 - bit UCS Transformation format
+            byte[] byteArr3 = str.getBytes("UTF-16LE");
+            System.out.println("After converted into UTF-16LE the String is : ");
+
+            for (int j = 0; j < byteArr3.length; j++)
+            {
+                System.out.print(byteArr3[j]);
+            }
+        }
+        catch (UnsupportedEncodingException g)
+        {
+            System.out.println("Unsupported character set" + g);
+        }
+    }
+}
