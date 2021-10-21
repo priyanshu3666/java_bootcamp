0731bc0e7e4763916fc79ea01a78b48060bd7ae5
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-21 16:56:33 +0530

    implement concept of constructor overloading

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/ConstructorOverloading.java
@@ -0,0 +1,29 @@
+package com.JavaBootCamp.ObjectOrientedProgramming;
+
+public class ConstructorOverloading {
+
+    public static void main(String[] args){
+        Student s1 = new Student(111,"Karan");
+        Student s2 = new Student(222,"Aryan",25);
+        s1.display();
+        s2.display();
+    }
+    static class Student {
+        int id;
+        String name;
+        int age;
+        //creating two arg constructor
+        Student(int i, String n){
+            id = i;
+            name = n;
+        }
+        //creating three arg constructor
+        Student(int i, String n, int a){
+            id = i;
+            name = n;
+            age=a;
+        }
+        void display(){System.out.println(id+" "+name+" "+age);}
+
+    }
+}
