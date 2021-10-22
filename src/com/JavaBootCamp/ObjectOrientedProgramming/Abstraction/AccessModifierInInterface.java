f1111645f13270d5743a01adb1325ce4274860ba
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-22 14:51:32 +0530

    implementing Access Modifier In Interface

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/Abstraction/AccessModifierInInterface.java
@@ -0,0 +1,22 @@
+package com.JavaBootCamp.ObjectOrientedProgramming.Abstraction;
+
+interface Drawable {
+    void draw();
+    default void msg() {
+        System.out.println("default method");
+    }
+}
+
+public class AccessModifierInInterface {
+    public static void main(String[] args) {
+        Drawable obj = new Rectangle();
+        obj.draw();
+        obj.msg();
+    }
+}
+
+class Rectangle implements Drawable {
+    public void draw() {
+        System.out.println("drawing rectangle");
+    }
+}
