e7155d0f2e03cffc86edfc62db1dc4de8f0d1141
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-22 15:49:32 +0530

    implementing package concept and importing

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/Encapsulation/DemoPackage2/Package2Class1.java
@@ -0,0 +1,10 @@
+package com.JavaBootCamp.ObjectOrientedProgramming.Encapsulation.DemoPackage2;
+import com.JavaBootCamp.ObjectOrientedProgramming.Encapsulation.DemoPackage1.*;
+
+public class Package2Class1 extends PackageClass1 {
+    public static void main(String args[]) {
+        Package2Class1 obj = new Package2Class1();
+        obj.msg();
+    }
+
+}
