f04e7c76f83b687dece4f44874dac49fce97239e
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-22 12:31:47 +0530

    implemented the downcasting of class object with  concept of instanceOf operator

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/Polymorphism/DownCastingWithInstanceOf.java
@@ -0,0 +1,22 @@
+package com.JavaBootCamp.ObjectOrientedProgramming.Polymorphism;
+
+public class DownCastingWithInstanceOf {
+
+    public static void main (String [] args) {
+        Animals a=new Dog3();
+        Dog3.method(a);
+    }
+
+}
+
+class Animals { }
+
+class Dog3 extends Animals {
+    static void method(Animals a) {
+        if(a instanceof Dog3){
+            Dog3 d=(Dog3)a;//downcasting
+            System.out.println("ok downcasting performed");
+        }
+    }
+
+}
