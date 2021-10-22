a614e7e3f1895d5c9a077a9b4688d285793d5ee4
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-22 15:33:51 +0530

    create display method

--- src/com/JavaBootCamp/Basic/Hello.java
+++ src/com/JavaBootCamp/Basic/Hello.java
@@ -1,7 +1,12 @@
 package com.JavaBootCamp.Basic;
 
 public class Hello {
+    public  void display(){System.out.println("Hello i am inside JavaBootcamp package inside basic");}
+
     public static void main(String[] args) {
-        System.out.println("com.JavaBootCamp.Basic.Hello");
+        Hello obj = new Hello();
+        obj.display();
+
     }
+
 }
