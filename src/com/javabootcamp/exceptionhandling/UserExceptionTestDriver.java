88842a53e335ebc5309676080d80c3c9f8579712
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-23 13:45:17 +0530

    implement user exceptions

---
+++ src/com/javabootcamp/exceptionhandling/UserExceptionTestDriver.java
@@ -0,0 +1,22 @@
+package com.javabootcamp.exceptionhandling;
+
+// class represents user-defined exception
+class UserDefinedException extends Exception {
+    public UserDefinedException(String str) {   // Calling constructor of parent Exception
+        super(str);
+    }
+}
+
+// Class that uses above MyException
+public class UserExceptionTestDriver {
+    public static void main(String[] args) {
+        try {
+            // throw an object of user defined exception
+            throw new UserDefinedException("This is user-defined exception");
+        } catch (UserDefinedException ude) {
+            System.out.println("Caught the exception");
+            // Print the message from MyException object
+            System.out.println(ude.getMessage());
+        }
+    }
+}
