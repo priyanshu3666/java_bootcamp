1bba6297b0355e476539f07b5c00d0329fc82bfe
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-23 13:45:51 +0530

    implement throw keyword

---
+++ src/com/javabootcamp/exceptionhandling/TestThrow.java
@@ -0,0 +1,27 @@
+package com.javabootcamp.exceptionhandling;
+
+public class TestThrow {
+    //function to check if person is eligible to vote or not
+    public static void validate(int age) {
+        try{
+            if(age<18) {
+                //throw Arithmetic exception if not eligible to vote
+                throw new ArithmeticException("Person is not eligible to vote");
+            }
+            else {
+                System.out.println("Person is eligible to vote!!");
+            }
+
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
+
+    }
+    //main method
+    public static void main(String args[]){
+        //calling the function
+        validate(13);
+        System.out.println("rest of the code...");
+    }
+}
