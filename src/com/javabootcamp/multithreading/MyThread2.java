4c334af1b5c1739db8257c651c4bd1e1aaddd7f5
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-25 14:46:46 +0530

    implementing  runnable interface  to main class

---
+++ src/com/javabootcamp/multithreading/MyThread2.java
@@ -0,0 +1,23 @@
+package com.javabootcamp.multithreading;
+
+public class MyThread2 implements Runnable
+{
+    public void run()
+    {
+        System.out.println("Now the thread is running ...");
+    }
+
+    public static void main(String[] args)
+    {
+
+        Runnable r1 = new MyThread2();
+
+        // creating an object of the class Thread using Thread(Runnable r, String name)
+        Thread th1 = new Thread(r1, "My new thread");
+        th1.start();
+
+        // getting the thread name by getName() method
+        String str = th1.getName();
+        System.out.println(str);
+    }
+}
